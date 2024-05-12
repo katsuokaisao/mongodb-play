30969ee4db4afa1c7f2c451e73078a5baf0fc4ac
katsuokaisao <isaoda0@icloud.com>
2024-05-12 11:15:16 +0900

    fix update one

--- cmd/update_one.go
+++ cmd/update_one.go
@@ -15,14 +15,37 @@ var updateOneCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		commentRepository := initMongoDB()
 		_id := "664020b5bef7a3d5e852e6c1"
-		fmt.Printf("Update by ID: %s\n", _id)
+
+		{
+			comment, err := commentRepository.FindOneByID(_id)
+			if err != nil {
+				log.Fatalf("failed to find one comment: %v", err)
+			}
+			if comment == nil {
+				fmt.Println("comment not found")
+				return
+			}
+			printJSON(comment)
+		}
+
 		field := repository.UpdateFiled{
-			Email: pointer.ToString("update2@example.com"),
+			Email: pointer.ToString("update3@example.com"),
 		}
 		err := commentRepository.UpdateOne(_id, field)
 		if err != nil {
 			log.Fatalf("failed to update one comment: %v", err)
 		}
-		fmt.Println("updated")
+
+		{
+			comment, err := commentRepository.FindOneByID(_id)
+			if err != nil {
+				log.Fatalf("failed to find one comment: %v", err)
+			}
+			if comment == nil {
+				fmt.Println("comment not found")
+				return
+			}
+			printJSON(comment)
+		}
 	},
 }
